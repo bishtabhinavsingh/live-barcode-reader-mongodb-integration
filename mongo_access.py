adbf789fccd49378a2c739cf0fc1a43a586724f5
Abhinav Singh Bisht <52534177+bishtabhinavsingh@users.noreply.github.com>
2022-04-02 20:31:34 -0400

    Update mongo_access.py

--- mongo_access.py
+++ mongo_access.py
@@ -3,7 +3,7 @@ client = MongoClient()
 
 
 def get_database():
-    CONNECTION_STRING = "mongodb://<user_name>:<password>@cluster0-shard-00-00.ncz48.mongodb.net:27017,cluster0-shard-00-01.ncz48.mongodb.net:27017,cluster0-shard-00-02.ncz48.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-rk9o8n-shard-0&authSource=admin&retryWrites=true&w=majority"
+    CONNECTION_STRING = "<CONNECTION STRING HERE>"
     client = MongoClient(CONNECTION_STRING)
     dbname = client['<data_base_name_here>']
     return dbname.<collection_name>
